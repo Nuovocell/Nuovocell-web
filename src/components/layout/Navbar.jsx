32922090729d98a88519fd9f15d3c56a0520ce63
Nuovocell <dev.nuovocell@gmail.com>
2026-03-16 12:02:08 -0400

    Update Navbar.jsx with real brand logos

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,9 +5,7 @@ import { useCartStore, useUIStore } from '../../lib/store';
 import './Navbar.css';
 
 const ShoppingBagIcon = () => (
-  <svg width="20" height="20" fill="none" stroke="currentColor" strokeWidth="1.8" viewBox="0 0 24 24">
-    <path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4z"/><line x1="3" y1="6" x2="21" y2="6"/><path d="M16 10a4 4 0 01-8 0"/>
-  </svg>
+  <img src="/logos/carrito.png" alt="Carrito" style={{ width: '22px', height: '22px', objectFit: 'contain', borderRadius: '50%' }} />
 );
 
 const MenuIcon = () => (
